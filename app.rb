375df5d7c0f8440054954cf8d337f19fadf094ab
Ross Allen <rossgit@gmail.com>
2015-01-11 21:37:45 -0800

    Add a fish demonstration starting at “/onefish”

--- app.rb
+++ app.rb
@@ -16,6 +16,25 @@ end
 get "/" do
   @panel_class = PANEL_CLASSES.sample
   @request_time = Time.now
-
   erb :index
+end
+
+get "/onefish" do
+  @fish_count = 1
+  erb :fish
+end
+
+get "/twofish" do
+  @fish_count = 2
+  erb :fish
+end
+
+get "/redfish" do
+  @fish_class = "fish-red"
+  erb :fish
+end
+
+get "/bluefish" do
+  @fish_class = "fish-blue"
+  erb :fish
 end
